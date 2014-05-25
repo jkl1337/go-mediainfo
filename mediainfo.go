dfd60321f3125cd6f018b6c1a34af79fb7808eaf
John K. Luebs <jkl@johnluebs.com>
2014-05-25 14:32:57 -0400

    docs(mediainfo): Add periods after headlines

--- mediainfo.go
+++ mediainfo.go
@@ -55,48 +55,48 @@ import (
 	"errors"
 )
 
-// MediaInfo is an instance of a mediainfo accessor
+// MediaInfo is an instance of a mediainfo accessor.
 type MediaInfo struct {
 	cptr unsafe.Pointer
 }
 
-// StreamKind is used to specify the type of stream (audio, video, chapters, etc) when getting information
+// StreamKind is used to specify the type of stream (audio, video, chapters, etc) when getting information.
 type StreamKind int
-// InfoKind is used to specify the aspect of information (name, value, unit of measure) when retrieving information
+// InfoKind is used to specify the aspect of information (name, value, unit of measure) when retrieving information.
 type InfoKind int
 
 const (
-	// StreamGeneral is a for general container stream
+	// StreamGeneral is a for general container stream.
 	StreamGeneral StreamKind = 0
-	// StreamVideo is a video stream
+	// StreamVideo is a video stream.
 	StreamVideo = 1
-	// StreamAudio is an audio stream
+	// StreamAudio is an audio stream.
 	StreamAudio = 2
-	// StreamText is embedded text (subtitles) stream
+	// StreamText is embedded text (subtitles) stream.
 	StreamText = 3
-	// StreamOther is for chapters
+	// StreamOther is for chapters.
 	StreamOther = 4
-	// StreamImage is for embedded images
+	// StreamImage is for embedded images.
 	StreamImage = 5
-	// StreamMenu is for dynamic menus
+	// StreamMenu is for dynamic menus.
 	StreamMenu = 6
 )
 
 const (
-	// InfoName is the unique name of parameter
+	// InfoName is the unique name of parameter.
 	InfoName InfoKind = 0
-	// InfoText is value of parameter
+	// InfoText is value of parameter.
 	InfoText = 1
-	// InfoMeasure is the unique name of measure unit of parameter
+	// InfoMeasure is the unique name of measure unit of parameter.
 	InfoMeasure = 2
 	InfoOptions = 3
-	// InfoNameText is translated name of parameter
+	// InfoNameText is translated name of parameter.
 	InfoNameText = 4
-	// InfoMeasureText is translated name of measure unit
+	// InfoMeasureText is translated name of measure unit.
 	InfoMeasureText = 5
-	// InfoInfo is more information about the parameter
+	// InfoInfo is more information about the parameter.
 	InfoInfo = 6
-	// InfoHowTo is how this parameter is supported, could be N (No), B (Beta), R (Read only), W (Read/write)
+	// InfoHowTo is how this parameter is supported, could be N (No), B (Beta), R (Read only), W (Read/write).
 	InfoHowTo = 7
 )
 
@@ -115,14 +115,14 @@ func emptyCString() *_Ctype_char {
 	return (*_Ctype_char)(unsafe.Pointer(&cEmptyString))
 }
 
-// ErrOpenFailed is returned by Open when mediainfo cannot open the file
+// ErrOpenFailed is returned by Open when mediainfo cannot open the file.
 var ErrOpenFailed = errors.New("file open failed")
 
 func init() {
 	C.MediaInfoDLL_Load()
 }
 
-// New initializes a MediaInfo handle
+// New initializes a MediaInfo handle.
 func New() *MediaInfo {
 	cmi := C.g_MediaInfo_New()
 	mi := &MediaInfo{cmi}
@@ -134,7 +134,7 @@ func New() *MediaInfo {
 	return mi
 }
 
-// Open opens the file at path with the mediainfo library
+// Open opens the file at path with the mediainfo library.
 func (mi *MediaInfo) Open(path string) error {
 	cpath := C.CString(path)
 	defer C.free(unsafe.Pointer(cpath))
@@ -168,12 +168,12 @@ func (mi *MediaInfo) Inform() string {
 	return C.GoString(C.g_MediaInfo_Inform(mi.cptr))
 }
 
-// Get gets the file info according to the parameters
+// Get gets the file info according to the parameters.
 func (mi *MediaInfo) Get(streamKind StreamKind, streamNumber int, parameter string) string {
 	return mi.GetKind(streamKind, streamNumber, parameter, InfoText)
 }
 
-// GetKind gets the file info according to the parameters
+// GetKind gets the file info according to the parameters.
 func (mi *MediaInfo) GetKind(streamKind StreamKind, streamNumber int, parameter string, kindOfInfo InfoKind) string {
 	cparameter := C.CString(parameter)
 	defer C.free(unsafe.Pointer(cparameter))
@@ -181,17 +181,17 @@ func (mi *MediaInfo) GetKind(streamKind StreamKind, streamNumber int, parameter 
 		toCInfo(kindOfInfo), toCInfo(InfoName)))
 }
 
-// GetAt gets the file info at a particular parameter index
+// GetAt gets the file info at a particular parameter index.
 func (mi *MediaInfo) GetAt(streamKind StreamKind, streamNumber int, parameter int, kindOfInfo InfoKind) string {
 	return C.GoString(C.g_MediaInfo_GetI(mi.cptr, toCStream(streamKind), C.size_t(streamNumber), C.size_t(parameter), toCInfo(kindOfInfo)))
 }
 
-// Count gets the count of stream
+// Count gets the count of stream.
 func (mi *MediaInfo) Count(streamKind StreamKind) int {
 	return int(C.g_MediaInfo_Count_Get(mi.cptr, toCStream(streamKind)))
 }
 
-// Close closes the handle, releasing internal resources
+// Close closes the handle, releasing internal resources.
 func (mi *MediaInfo) Close() {
 	C.g_MediaInfo_Close(mi.cptr)
 }
